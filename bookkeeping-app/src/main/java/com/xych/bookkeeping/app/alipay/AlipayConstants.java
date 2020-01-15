c6bdddea36a857f120118fc31ba5811c92559fd0
Lanboo <549434510@qq.com>
2020-01-15 10:45:54 +0800

    UPDATE 优化常量

--- bookkeeping-app/src/main/java/com/xych/bookkeeping/app/alipay/AlipayConstants.java
+++ bookkeeping-app/src/main/java/com/xych/bookkeeping/app/alipay/AlipayConstants.java
@@ -2,6 +2,11 @@ package com.xych.bookkeeping.app.alipay;
 
 public class AlipayConstants {
     public static final String CHROME_INSTALLATION_PATH = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+    // public static final String CHROME_INSTALLATION_PATH = "C:\\Users\\weixw\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe";
+    /**
+     * chrome的用户配置文件，防止污染自己的chrome配置
+     */
+    public static final String CHROME_USER_DATA_DIR = "D:\\data\\selenum\\chrome\\AutomationProfile";
     public static final String CHROME_DRIVER_PATH = "D:\\data\\selenum\\chromedriver.exe";
     public static final String PATH_LOGIN = "https://auth.alipay.com/login/index.htm";
     public static final String PATH_RECORD = "https://consumeprod.alipay.com/record/index.htm";
--- bookkeeping-app/src/main/java/com/xych/bookkeeping/app/alipay/WebDriverFactory.java
+++ bookkeeping-app/src/main/java/com/xych/bookkeeping/app/alipay/WebDriverFactory.java
@@ -11,10 +11,6 @@ public class WebDriverFactory {
      * 启动端口号
      */
     private static final String PORT = "9999";
-    /**
-     * chrome的用户配置文件，防止污染自己的chrome配置
-     */
-    private static final String CHROME_USER_DATA_DIR = "D:\\data\\logs\\selenum\\chrome\\AutomationProfile";
 
     private WebDriverFactory() {
     }
@@ -25,7 +21,7 @@ public class WebDriverFactory {
             ChromeOptions chromeOptions = new ChromeOptions();
             if(cmd) {
                 // 利用cmd，加载chrome.exe，指定端口号、用户目录
-                String cmdStr = AlipayConstants.CHROME_INSTALLATION_PATH + " --remote-debugging-port=" + PORT + " --user-data-dir=\"" + CHROME_USER_DATA_DIR + "\"";
+                String cmdStr = AlipayConstants.CHROME_INSTALLATION_PATH + " --remote-debugging-port=" + PORT + " --user-data-dir=\"" + AlipayConstants.CHROME_USER_DATA_DIR + "\"";
                 Runtime.getRuntime().exec(cmdStr);
                 // 利用 Chrome DevTools 协议: https://blog.csdn.net/freeking101/article/details/94601986
                 chromeOptions.setExperimentalOption("debuggerAddress", "127.0.0.1:" + PORT);
