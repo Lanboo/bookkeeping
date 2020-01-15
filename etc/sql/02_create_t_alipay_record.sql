4ed5df6474617093268078cfb53032ac817fdc49
Lanboo <549434510@qq.com>
2020-01-15 11:44:49 +0800

    UPDATE sql增加Comment

--- etc/sql/02_create_t_alipay_record.sql
+++ etc/sql/02_create_t_alipay_record.sql
@@ -1,7 +1,7 @@
 CREATE TABLE t_alipay_record
 (
-    id BIGINT NOT NULL,
-    user_code VARCHAR(32),
+    id BIGINT NOT NULL COMMENT 'id',
+    user_code VARCHAR(32) COMMENT '用户代码',
     consume_time CHAR(14) COMMENT '交易时间',
     consume_title VARCHAR(128) COMMENT '标题',
     trade_no VARCHAR(64) COMMENT '商户订单号',
@@ -12,9 +12,9 @@ CREATE TABLE t_alipay_record
     status VARCHAR(16) COMMENT '状态',
     fund_tool VARCHAR(16) COMMENT '流入流出账户',
     fund_tool_from VARCHAR(16) COMMENT '流出账户',
-    crt_time DATETIME,
-    upt_time DATETIME,
-    operator VARCHAR(32),
+    crt_time DATETIME COMMENT '更新时间',
+    upt_time DATETIME COMMENT '修改时间',
+    operator VARCHAR(32) COMMENT '操作人',
     PRIMARY KEY (ID),
     index idx_t_alipay_record (consume_time,trade_id)
 ) COMMENT '支付宝流水记录';
