f73d606b7400d232a875e6ace61609a17293f42b
Lanboo <549434510@qq.com>
2020-02-21 22:15:47 +0800

    UPDATE Asset @Table

--- bookkeeping-dao/src/main/java/com/xych/bookkeeping/dao/entity/Asset.java
+++ bookkeeping-dao/src/main/java/com/xych/bookkeeping/dao/entity/Asset.java
@@ -11,7 +11,7 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-@Table(name = "t_account")
+@Table(name = "t_asset")
 public class Asset extends BaseEntity {
     private static final long serialVersionUID = 1L;
     /**
