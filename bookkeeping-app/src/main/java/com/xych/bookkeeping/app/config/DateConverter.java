3e40c8460aa2dc0606d1cd9d23c4ad0bf8a2e99e
Lanboo <549434510@qq.com>
2020-03-02 00:25:39 +0800

    UPDATE 优化代码

--- bookkeeping-app/src/main/java/com/xych/bookkeeping/app/config/DateConverter.java
+++ bookkeeping-app/src/main/java/com/xych/bookkeeping/app/config/DateConverter.java
@@ -1,28 +1,33 @@
 package com.xych.bookkeeping.app.config;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DateConverter implements Converter<String, Date> {
-    private static final List<String> formarts = new ArrayList<>(8);
+    private static final String[] parsePatterns;
     static {
+        List<String> formarts = new ArrayList<>(12);
         formarts.add("yyyy-MM");
         formarts.add("yyyy-MM-dd");
         formarts.add("yyyy-MM-dd HH:mm");
         formarts.add("yyyy-MM-dd HH:mm:ss");
+        formarts.add("yyyy-MM-dd HH:mm:ss.SSS");
+        formarts.add("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
         formarts.add("yyyyMM");
         formarts.add("yyyyMMdd");
         formarts.add("yyyyMMddHHmm");
         formarts.add("yyyyMMddHHmmss");
+        formarts.add("yyyyMMddHHmmssSSS");
         formarts.add("yyyyMMdd HHmm");
         formarts.add("yyyyMMdd HHmmss");
+        parsePatterns = formarts.toArray(new String[0]);
     }
 
     @Override
@@ -31,55 +36,11 @@ public class DateConverter implements Converter<String, Date> {
         if("".equals(value)) {
             return null;
         }
-        if(source.matches("^\\d{4}-\\d{1,2}$")) {
-            return parseDate(source, formarts.get(0));
-        }
-        else if(source.matches("^\\d{4}-\\d{1,2}-\\d{1,2}$")) {
-            return parseDate(source, formarts.get(1));
-        }
-        else if(source.matches("^\\d{4}-\\d{1,2}-\\d{1,2} {1}\\d{1,2}:\\d{1,2}$")) {
-            return parseDate(source, formarts.get(2));
-        }
-        else if(source.matches("^\\d{4}-\\d{1,2}-\\d{1,2} {1}\\d{1,2}:\\d{1,2}:\\d{1,2}$")) {
-            return parseDate(source, formarts.get(3));
-        }
-        else if(source.matches("^\\d{6}$")) {
-            return parseDate(source, formarts.get(4));
-        }
-        else if(source.matches("^\\d{8}")) {
-            return parseDate(source, formarts.get(5));
-        }
-        else if(source.matches("^\\d{12}$")) {
-            return parseDate(source, formarts.get(6));
-        }
-        else if(source.matches("^\\d{14}$")) {
-            return parseDate(source, formarts.get(7));
-        }
-        else if(source.matches("^\\d{8} {1}\\d{4}")) {
-            return parseDate(source, formarts.get(8));
-        }
-        else if(source.matches("^\\d{8} {1}\\d{6}")) {
-            return parseDate(source, formarts.get(9));
-        }
-        else {
-            throw new IllegalArgumentException("Invalid boolean value '" + source + "'");
-        }
-    }
-
-    /**
-     * 格式化日期
-     * @param dateStr String 字符型日期
-     * @param format String 格式
-     * @return Date 日期
-     */
-    private Date parseDate(String dateStr, String format) {
-        Date date = null;
         try {
-            DateFormat dateFormat = new SimpleDateFormat(format);
-            date = dateFormat.parse(dateStr);
+            return DateUtils.parseDate(source, parsePatterns);
         }
-        catch(Exception e) {
+        catch(ParseException e) {
+            throw new IllegalArgumentException("Invalid boolean value '" + source + "'", e);
         }
-        return date;
     }
 }
